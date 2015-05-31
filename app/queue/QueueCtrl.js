eb5701a38f12f7594a5b38a5b8fed5abd9fc9498
Brian McCarthy <brian.mcc@rthy.org>
2015-05-31 10:20:10 -0400

    minor bug fix

--- app/queue/QueueCtrl.js
+++ app/queue/QueueCtrl.js
@@ -1,7 +1,26 @@
 (function () {
     'use strict';
 
-    angular.module('app').controller('QueueCtrl', ['$scope', 'jobs', function ($scope, jobs) {
+    angular.module('app').controller('QueueCtrl', ['$scope', '$http', 'jobs', function ($scope, $http, jobs) {
+        var offset = 0;
+
         $scope.items = jobs;
+
+        $scope.loadMore = function () {
+            offset += 10;
+            loadItems();
+        };
+
+        $scope.refresh = function () {
+            offset = 0;
+            $scope.items = [];
+            loadItems();
+        };
+
+        function loadItems() {
+            $http.get('/api/google/printJobs?showQueued=true&offset=' + offset).success(function (jobs) {
+                $scope.items = $scope.items.concat(jobs);
+            });
+        }
     }]);
 })();
