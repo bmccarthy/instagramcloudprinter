eca63b90936337c04156841374d80d737b19dd81
Brian McCarthy <brian.mcc@rthy.org>
2015-12-24 12:45:05 -0500

    adding logging statements.

--- server/printer.js
+++ server/printer.js
@@ -19,7 +19,7 @@
 
         mkdirp(path.dirname(filepath), function (err) {
             if (err) {
-                config.logger('error while creating path');
+                config.logger.info('error while creating path');
                 config.logger.error(err);
                 return;
             }
